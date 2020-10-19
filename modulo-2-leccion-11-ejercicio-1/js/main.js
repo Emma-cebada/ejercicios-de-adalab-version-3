dd7c97651a34cf02c7a5a00cd8f5cc9e824e2cc5
Emma <emma.cebada@gmail.com>
2020-10-19 21:19:49 +0200

    Add aswers to exercise 1 in js file

---
+++ modulo-2-leccion-11-ejercicio-1/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//1-la URL base del API: https://swapi.dev/api/
+//2-si necesita autenticación: No, no la necesita. Es una API abierta.
+//3-método HTTP que deben usar las peticiones a este API: GET
+//4-URL para acceder a la info del personaje "Luke Skywalker": https://swapi.dev/api/people/1
+//5-URL para acceder a la info de la película "A New Hope": https://swapi.dev/api/films/1/
+//6-a qué otros recursos puedo acceder desde el API además de personajes y pelis: planet, species, vehicles and startships.
+//7-URL para acceder al listado de personajes, ¿está paginada?: No
+//8-cómo puedo buscar personajes mediante la URL: https://swapi.dev/api/people/ (name)
+//9-cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee: añadiendo ?format=wookiee al final de las URLs de petición-->
