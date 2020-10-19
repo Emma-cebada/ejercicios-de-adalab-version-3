6866f0ba693e7b61c023d7a201519dd49d05f1fc
Emma <emma.cebada@gmail.com>
2020-10-19 21:37:17 +0200

    Add some code lines

---
+++ modulo-2-leccion-11-ejercicio-2/js/main.js
@@ -0,0 +1,9 @@
+const characterName = document.querySelector(".js-inputName");
+const inputButton = document.querySelector(".js-inputButton");
+const characterResult = document.querySelector(".js-characterList");
+
+
+
+
+
+inputButton.addEventListener("click", getResult);
