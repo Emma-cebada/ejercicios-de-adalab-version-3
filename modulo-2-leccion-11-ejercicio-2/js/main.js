f919d613ba8e31c8ded15abb79d87a8a3ec83f4a
Emma <emma.cebada@gmail.com>
2020-10-22 10:50:42 +0200

    Add exercise finished

--- modulo-2-leccion-11-ejercicio-2/js/main.js
+++ modulo-2-leccion-11-ejercicio-2/js/main.js
@@ -1,9 +1,18 @@
-const characterName = document.querySelector(".js-inputName");
-const inputButton = document.querySelector(".js-inputButton");
-const characterResult = document.querySelector(".js-characterList");
+"use strict";
 
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const result = document.querySelector(".js-result");
 
+function clickButton() {
+  let inputValue = input.value;
+  fetch(`https://swapi.dev/api/people/?search=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (const character of data.results) {
+        result.innerHTML += `<li>Tu personaje es ${character.name} y su género es ${character.gender}</li>`;
+      }
+    });
+}
 
-
-
-inputButton.addEventListener("click", getResult);
+button.addEventListener("click", clickButton);
