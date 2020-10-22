2fdd415514e7dcfa7879ad0de26e945d5d455b45
Emma <emma.cebada@gmail.com>
2020-10-22 13:32:44 +0200

    Add repaso final terminado

--- modulo-2-repaso-final-parte-1/js/main.js
+++ modulo-2-repaso-final-parte-1/js/main.js
@@ -2,6 +2,7 @@
 
 const palettesContainer = document.querySelector(".js-palettes-container");
 let palettes = [];
+let favoritesLis = [];
 
 function getData() {
   fetch("https://beta.adalab.es/ejercicios-extra/js-ejercicio-de-paletas/data/palettes.json")
@@ -11,21 +12,68 @@ function getData() {
     .then(function (data) {
       palettes = data.palettes;
       paintPalettes(); //Esta función tiene que ser llamada aquí pq en este punto es cuando ya tengo la información recogida y transformada al json.
+      listenPalettes();
+      setLocalStorage();
     })
 }
 
 function paintPalettes() {
   let html = ""; //Esto tiene que ir fuera del for, para que cada vez se pinte una y no ocurra que las va machacando y se queda sólo con una.
-  for (const palette of palettes) {
-    html += "<li>";
-    html += `<h2 class="palettes__name">${palette.name}</h2>`;
+  for (let i = 0; i < palettes.length; i++) {
+    let classF;
+    const favoriteIndex = favoritesLis.indexOf(i);
+    const favorite = favoriteIndex !== -1;
+    if (favorite === true) {
+      classF = "palettes__item--favorite";
+    } else {
+      classF = "";
+    }
+    html += `<li class="js-palette-item ${classF}" id="${i}">`;
+    html += `<h2 class="palettes__name">${palettes[i].name}</h2>`;
     html += `<div class="palettes__colors">`;
-    for (const color of palette.colors) {
+    for (const color of palettes[i].colors) {
       html += `<div class="palettes__color" style="background-color:#${color}"></div>`;
     }
     html += "</div>";
     html += "</li>";
   }
   palettesContainer.innerHTML = html;
+}
+
+const favoritePalette = function (event) {
+  const clicked = parseInt(event.currentTarget.id);
+  // const isFavorite = favoritesLis.find(click => { return click });
+  const indexFav = favoritesLis.indexOf(clicked);
+  const isFavorite = indexFav !== -1;
+  if (isFavorite === false) {
+    favoritesLis.push(clicked);
+  } else {
+    favoritesLis.splice(indexFav, 1);
+  }
+  paintPalettes();
+  listenPalettes();
+}
+
+function listenPalettes() {
+  const paletteItems = document.querySelectorAll(".js-palette-item"); //Esta constante tiene que estar dentro de la función pq es de un array que se crea después de la respuesta del servidor.
+  for (const paletteItem of paletteItems) {
+    paletteItem.addEventListener("click", favoritePalette);
+  }
+}
+
+function setLocalStorage() {
+  localStorage.setItem("palettes", JSON.stringify(palettes));
+}
+
+function getLocalStorage() {
+  const localPalette = localStorage.getItem("palettes");
+  const localPaletteJ = JSON.parse(localPalette);
+  if (localPaletteJ === null) {
+    getData();
+  } else {
+    palettes = localPaletteJ;
+    paintPalettes();
+    listenPalettes();
+  }
 }
 getData();
--- modulo-2-repaso-final-parte-1/readme.md
+++ modulo-2-repaso-final-parte-1/readme.md
@@ -1,4 +1,6 @@
-Paso a seguir para resolver el ejercicio (es el ejercicio de repaso final del modulo 2) -[x] Pintar una paleta -[x] Fetch para obtener datos -[x] Tranformar los datos (parsear) del servidor -[x] Pintar la paleta en el html -[x] Ponerlo bonito con el css -[x] Pintar muchas paletas
--[] Marcar como favoritas la seleccionada por la usuaria
+Paso a seguir para resolver el ejercicio (es el ejercicio de repaso final del modulo 2 Paletas) -[x] Pintar una paleta -[x] Fetch para obtener datos -[x] Tranformar los datos (parsear) del servidor -[x] Pintar la paleta en el html -[x] Ponerlo bonito con el css -[x] Pintar muchas paletas
+-[] Marcar como favoritas la seleccionada por la usuaria -[x] Escuchar el evento click en cada nave -[x] Identificar el elemento clickado -[x] Guardar los favoritos en un array
+-[] Añadir la clase de favoritos
+-[] Quitar la clase de favoritos
 -[] Añadir el filtro por nombre
 -[] localStorage
